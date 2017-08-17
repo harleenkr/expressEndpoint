417733b1260983b645b040aaa766b369fdf88dab
Harleen Kaur <harleen.kaur212@gmail.com>
2017-08-17 17:57:54 +0530

    type(feat): added console msg in index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 var express = require('express');
 var app = express();
 var Docker = require('dockerode');
+
+app.get('/',function(req,res){
+res.send("initally check GET details");
+});
+
+app.listen(3030);
