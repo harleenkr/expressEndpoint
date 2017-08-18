57e46f5c91ae4c27e134d7b911dce3c924f715a7
Harleen Kaur <harleen.kaur212@gmail.com>
2017-08-18 11:07:01 +0530

    type(fix): removed res.send() from controllers.js as it was initally used for testing the resonse of endpoint

--- controllers.js
+++ controllers.js
@@ -5,13 +5,11 @@ const listContainers = async function(req, res){
 const listContainer = await docker.listContainers();
 console.log(listContainer)
     res.json(listContainer);
-    res.send("initally check GET details");
 }
 
 const listImages = async function(req, res){
   const listImage = await docker.listImages();
     res.json(listImage);
-    res.send("check Image details");
 } 
 
 exports.images = listImages;
